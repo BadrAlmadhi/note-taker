bfb17abb9b3578d8fbe48138274f71e23a683d0a
Badr Almadi <badr.almadhi95@gmail.com>
2023-05-17 20:37:02 -0600

    adding the post in fs file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,11 +7,15 @@ router.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)) )
 });
 
-router.post('/notes', (req,res)=>{
-    writeToFile('./db/db.json').then((data) => res.json(JSON.parse(data)) )
-});
+router.post("/notes", (req, res) => {
+    const { title, text } = req.body;
+    const newNote = { title, text };
+    writeToFile("./db/db.json", newNote);
+  });
 
 router.delete('/notes', (req,res)=>{
 readAndAppend('./db/db.json').then((data) => res.json(JSON.parse(data)) )
 });
+
+
 module.exports = router;
