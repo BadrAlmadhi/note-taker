531ad33a4fee8c24a7172808ecb73607aa67747a
Badr Almadi <badr.almadhi95@gmail.com>
2023-05-19 11:18:18 -0600

    adding delete route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 //const fs = require('fs');
 const {readFromFile, writeToFile, readAndAppend} = require('../db/utils');
-
+const { v4: uuidv4 } = require('uuid');
 
 router.get('/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)) )
@@ -9,10 +9,18 @@ router.get('/notes', (req, res) => {
 
 router.post("/notes", (req, res) => {
     const { title, text } = req.body;
-    const newNote = { title, text };
+    const newNote = { title, text, id: uuidv4() };
     return res.json(readAndAppend(newNote, "./db/db.json"));
   });
 
+  router.delete("/notes/:id", (req, res) => {
+    fetch(`/api/notes/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+  });
  
 
 module.exports = router;
